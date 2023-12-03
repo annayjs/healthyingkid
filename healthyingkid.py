4ad8d0cdf80258b2ff4d7ee550a5bd0e6c372fba
annayjs <102045970+annayjs@users.noreply.github.com>
2023-12-04 03:06:57 +0900

    Update healthyingkid.py

--- healthyingkid.py
+++ healthyingkid.py
@@ -289,15 +289,15 @@ elif selection == "menu2":
         child_data = pd.read_csv('child_info.csv')
         child_name_list=child_data['name'].to_list()
 
-        child_choice = st.radio("아이를 선택하세요:", (child_name_list))
-        child_idx=child_name_list.index(child_choice)
+        child_choice_2 = st.radio("아이를 선택하세요:", (child_name_list))
+        child_idx=child_name_list.index(child_choice_2)
 
-        if child_choice is not None:
-            st.write(f"Day 1✏️: \n {child_data[child_data['name']==child_choice].loc[:, 'Day 1'][child_idx]}")
+        if child_choice_2 is not None:
+            st.write(f"Day 1✏️: \n {child_data[child_data['name']==child_choice_2].loc[:, 'Day 1'][child_idx]}")
             st.write("_________________________________________________________________________________________________________")
-            st.write(f"Day 2✏️: \n{child_data[child_data['name']==child_choice].loc[:, 'Day 2'][child_idx]}")
+            st.write(f"Day 2✏️: \n{child_data[child_data['name']==child_choice_2].loc[:, 'Day 2'][child_idx]}")
             st.write("_________________________________________________________________________________________________________")
-            st.write(f"Day 3✏️: \n{child_data[child_data['name']==child_choice].loc[:, 'Day 3'][child_idx]}")
+            st.write(f"Day 3✏️: \n{child_data[child_data['name']==child_choice_2].loc[:, 'Day 3'][child_idx]}")
             
 
 ###################################################################################################################
