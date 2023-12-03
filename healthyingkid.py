163c1b3d853c5c4f3ef6fa4e9a859c7d4f890537
annayjs <102045970+annayjs@users.noreply.github.com>
2023-12-04 00:25:56 +0900

    Update healthyingkid.py

--- healthyingkid.py
+++ healthyingkid.py
@@ -269,9 +269,11 @@ elif selection == "menu2":
         child_choice = st.radio("아이를 선택하세요:", (child_name_list))
         child_idx=child_name_list.index(child_choice)
         
-        st.write(f"Day 1: {child_data[child_data['name']==child_choice].loc[:, 'Day 1'][child_idx]}")
-        st.write(f"Day 2: {child_data[child_data['name']==child_choice].loc[:, 'Day 2'][child_idx]}")
-        st.write(f"Day 3: {child_data[child_data['name']==child_choice].loc[:, 'Day 3'][child_idx]}")
+        st.write(f"Day 1✏️: \n {child_data[child_data['name']==child_choice].loc[:, 'Day 1'][child_idx]}")
+        st.write("")
+        st.write(f"Day 2✏️: \n{child_data[child_data['name']==child_choice].loc[:, 'Day 2'][child_idx]}")
+        st.write("")
+        st.write(f"Day 3✏️: \n{child_data[child_data['name']==child_choice].loc[:, 'Day 3'][child_idx]}")
             
 
 ###################################################################################################################
