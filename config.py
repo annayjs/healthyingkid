a3cef44b2ee4d6eb00381abb8fbc21367cbb4c00
annayjs <102045970+annayjs@users.noreply.github.com>
2023-12-03 22:51:17 +0900

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-OPENAI_API_KEY = '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'
+OPENAI_API_KEY = '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'
 DeepL_API_KEY = 'c24af978-e422-0d8b-4420-4c2daa1a067e:fx' 
